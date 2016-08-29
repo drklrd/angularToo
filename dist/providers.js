f15a0d04723047bf3fdd69fe56de46bfa9ddded7
Saurav <saurav@zyoba.com>
2016-08-29 18:01:15 +0545

    adds dist in gitignore

--- dist/providers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var core_1 = require('@angular/core');
-exports.LOOKUP_LISTS = new core_1.OpaqueToken('LookupLists');
-exports.lookupLists = {
-    itemTypes: ['Electronics', 'Home Appliances', 'Health Care', 'Automobiles']
-};
-//# sourceMappingURL=providers.js.map
